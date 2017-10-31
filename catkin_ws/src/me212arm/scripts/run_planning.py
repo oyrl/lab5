57539da38a35b79995c75a4afe4bf5ebf18aef3e
oyrl <oyrl.eed06g@nctu.edu.tw>
2017-10-31 23:16:16 +0800

    add the calclation of target_xz, but it seems a little strange

--- catkin_ws/src/me212arm/scripts/run_planning.py
+++ catkin_ws/src/me212arm/scripts/run_planning.py
@@ -21,12 +21,15 @@ if __name__=="__main__":
     
     robotjoints = rospy.wait_for_message('/joint_states', sensor_msgs.msg.JointState)
     q0 = robotjoints.position
-    
+
     for theta in np.linspace(0, 4*np.pi):
         # set up waypoints (in this case circular)
         # known parameters: theta, radius, center[0], center[1]
         # target_xz=???
-        
+        #target_xz = [center[0] + radius * np.cos(theta), center[1] + radius * np.sin(theta)]
+        target_xz = [center[0] + radius * np.cos(theta), center[1] + radius * np.sin(theta)]
+#        print "q0", q0
+#        print "target", target_xz
         
         q_sol = planner.ik(target_xz, q0)
         if q_sol is None:
